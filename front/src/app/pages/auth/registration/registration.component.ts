bf8029f0870922620044e187a230e89b38172990
Abdulaziz <mannopov481@gmail.com>
2022-06-21 12:39:29 +0500

    added validators in registration page, changed alert service

--- front/src/app/pages/auth/registration/registration.component.ts
+++ front/src/app/pages/auth/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ConfirmPasswordValidator} from '../../../shared/helpers/confirmPassword.validator'
 
 @Component({
   selector: 'app-registration',
@@ -7,7 +9,38 @@ import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RegistrationComponent implements OnInit {
-  constructor() { }
+  public form: FormGroup = new FormGroup({})
+
+  constructor(private fb: FormBuilder) {
+    this.form = fb.group({
+      firstName: ['', [Validators.required]],
+      lastName: '',
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ''
+    }, {
+      validator: ConfirmPasswordValidator('password', 'confirmPassword')
+    })
+  }
+
+  get regForm(){
+    return this.form.controls;
+  }
+
+  public isDisabled(): boolean {
+    return (
+      this.regForm['firstName']?.errors?.['required'] ||
+      this.regForm['confirmPassword']?.errors?.['confirmedValidator'] ||
+      this.regForm['password']?.errors?.['required'] ||
+      this.regForm['password']?.errors?.['minlength'] ||
+      this.regForm['email']?.errors?.['required'] ||
+      this.regForm['email']?.errors?.['email']
+    )
+  }
+
+  public registration(): void {
+    console.log(this.regForm)
+  }
 
   ngOnInit(): void {
   }
