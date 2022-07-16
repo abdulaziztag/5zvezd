bc52d3bb543dc9e32720e4a5971834e4bda235b0
Abdulaziz <mannopov481@gmail.com>
2022-07-16 21:57:21 +0500

    added admin rights endpoint to user controller, added guards to user routes

--- back/routes/userRoutes.js
+++ back/routes/userRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express';
 const router = express.Router();
+import passport from 'passport';
 import {upload} from '../utils/multer.util.js';
-import {uploadAvatar, changeSettings} from '../controllers/user.js';
+const auth = passport.authenticate('jwt', { session: false });
+import {uploadAvatar, changeSettings, adminRights} from '../controllers/user.js';
 
-router.post('/upload', upload.single('file'), uploadAvatar);
-router.post('/change', changeSettings);
+router.post('/upload', [auth, upload.single('img')], uploadAvatar);
+router.post('/change', auth, changeSettings);
+router.post('/admin', auth, adminRights);
 
 export const userRoutes = router;
