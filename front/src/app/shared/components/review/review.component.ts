88f9d4ff7a6b8b520ba6e077f17b9c3bb06afba7
Abdulaziz <mannopov481@gmail.com>
2022-07-26 00:33:19 +0500

    changed comment interface, and comment img

--- front/src/app/shared/components/review/review.component.ts
+++ front/src/app/shared/components/review/review.component.ts
@@ -1,8 +1,8 @@
 import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
 import {CommentInterface} from "../../interfaces/comment.interface";
-import {base64ArrayBuffer} from "../../helpers/base64ArrayBuffer.function";
 import {MatDialog} from "@angular/material/dialog";
 import {DeleteCommentDialogComponent} from '../delete-comment-dialog/delete-comment-dialog.component';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-review',
@@ -13,21 +13,21 @@ import {DeleteCommentDialogComponent} from '../delete-comment-dialog/delete-comm
 export class ReviewComponent implements OnInit {
   @Input() comment: CommentInterface;
   @Input() yourComment: boolean;
-  public convertedBase64: string = ''
   public starId: number[] = [1, 2, 3, 4, 5];
   constructor(
     private dialog: MatDialog,
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit(): void {
-    if (this.comment.userInfo[0]?.img) {
-      this.convertedBase64 = base64ArrayBuffer(this.comment.userInfo[0]?.img?.data)
-    }
   }
 
   public deleteComment(): void {
     this.dialog.open(DeleteCommentDialogComponent, {
-      disableClose: true
+      disableClose: true,
+      data: {
+        productId: this.route.snapshot.params['productId']
+      }
     })
   }
 }
