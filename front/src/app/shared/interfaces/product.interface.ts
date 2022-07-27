8e6a1137aff958464cdff2d00228f5015babc906
Abdulaziz <mannopov481@gmail.com>
2022-07-28 00:15:56 +0500

    added FilteredProductsInterface

--- front/src/app/shared/interfaces/product.interface.ts
+++ front/src/app/shared/interfaces/product.interface.ts
@@ -16,3 +16,15 @@ export interface ProductCardInterface {
   averageRating: number
   _id: string
 }
+
+export interface FilteredProductsInterface {
+  title: string,
+  averageRating: number,
+  img: string,
+  company: string,
+  category: string,
+  minCost: number,
+  maxCost: number,
+  createdAt: number,
+  _id: string
+}
