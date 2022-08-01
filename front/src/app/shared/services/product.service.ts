99136dabb774fe1f473ad150e6b991f28e15dca3
Abdulaziz <mannopov481@gmail.com>
2022-08-01 23:06:11 +0500

    fixed bug while rating doesn't change after deleting and adding review

--- front/src/app/shared/services/product.service.ts
+++ front/src/app/shared/services/product.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from "@angular/core";
 import {FilteredProductsInterface, ProductCardInterface, ProductInterface} from "../interfaces/product.interface";
-import {Observable, Subject} from "rxjs";
+import {BehaviorSubject, Observable, Subject} from "rxjs";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {environment} from "../../../environments/environment";
 
@@ -14,7 +14,7 @@ const httpOptions = {
 })
 
 export class ProductService {
-  public product$ = new Subject<ProductInterface>();
+  public product$ = new BehaviorSubject<ProductInterface>(null);
   public recentlyAdded$ = new Subject<ProductCardInterface[]>()
   public popular$ = new Subject<ProductCardInterface[]>()
   public mostExpensive$ = new Subject<ProductCardInterface[]>()
@@ -97,4 +97,8 @@ export class ProductService {
     this.carouselSlides$.next(products)
   }
 
+  public changeRating(averageRating: number): void {
+    this.product$.getValue()
+    this.product$.next({...this.product$.getValue(), averageRating})
+  }
 }
