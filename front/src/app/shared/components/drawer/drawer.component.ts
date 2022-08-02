53426d4916a8b45d62ea77dd92607a8a64522e18
Abdulaziz <mannopov481@gmail.com>
2022-08-02 12:57:27 +0500

    implemented uploading avatar and added spinner while uploading

--- front/src/app/shared/components/drawer/drawer.component.ts
+++ front/src/app/shared/components/drawer/drawer.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {DrawerService} from "../../services/drawer.service";
 import {TabInterface} from "../../interfaces/tab.interface";
 import {TokenStorageService} from "../../services/token-storage.service";
@@ -7,6 +7,7 @@ import {Router} from "@angular/router";
 import {MatDialog} from "@angular/material/dialog";
 import {UserSettingsDialogComponent} from "../user-settings-dialog/user-settings-dialog.component";
 import {UserService} from "../../services/user.service";
+import {Subject, takeUntil} from "rxjs";
 
 @Component({
   selector: 'app-drawer',
@@ -15,8 +16,10 @@ import {UserService} from "../../services/user.service";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DrawerComponent implements OnInit, OnDestroy {
-  @Input() tabs: TabInterface[] = []
-  public avatar: string = ''
+  @Input() tabs: TabInterface[] = [];
+  public avatar: string = '';
+  private notifier = new Subject<void>();
+  public loader: boolean = false;
 
   constructor(
     public drawerService: DrawerService,
@@ -24,14 +27,19 @@ export class DrawerComponent implements OnInit, OnDestroy {
     private alertService: AlertService,
     public router: Router,
     private dialog: MatDialog,
-    public userService: UserService
-  ) { }
+    public userService: UserService,
+    private ref: ChangeDetectorRef
+  ) {
+  }
 
   ngOnInit(): void {
-    this.userService.getAvatar().subscribe(data => {
-      this.avatar = data
-    })
-    this.userService.setAvatar(this.tokenService.getUser().img)
+    this.userService.getAvatar()
+      .pipe(takeUntil(this.notifier))
+      .subscribe(data => {
+        this.avatar = data;
+        this.ref.markForCheck();
+      });
+    this.userService.setAvatar(this.tokenService.getUser().img);
   }
 
   public close(): void {
@@ -39,8 +47,16 @@ export class DrawerComponent implements OnInit, OnDestroy {
   }
 
   public changeAvatar(event: Event): void {
+    this.loader = true
     const file = (event.target as HTMLInputElement).files[0];
-    console.log(file)
+    this.userService.uploadAvatar(file)
+      .pipe(takeUntil(this.notifier))
+      .subscribe((data) => {
+        this.tokenService.saveUser({...this.tokenService.getUser(), img: data.img})
+        this.loader = false
+        this.userService.setAvatar(data.img)
+        this.alertService.openSnackBar(data.message)
+      })
   }
 
   public signOut(): void {
@@ -57,6 +73,8 @@ export class DrawerComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
+    this.notifier.next()
+    this.notifier.complete()
   }
 
 }
