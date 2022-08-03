4d6c1590792c1a3e8aa311350558f4d09738e6d8
Abdulaziz <mannopov481@gmail.com>
2022-08-03 23:41:44 +0500

    added account-menu component and rendered in header and drawer

--- front/src/app/shared/components/drawer/drawer.component.ts
+++ front/src/app/shared/components/drawer/drawer.component.ts
@@ -1,13 +1,9 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnInit} from '@angular/core';
 import {DrawerService} from "../../services/drawer.service";
 import {TabInterface} from "../../interfaces/tab.interface";
 import {TokenStorageService} from "../../services/token-storage.service";
-import {AlertService} from "../../services/alert.service";
 import {Router} from "@angular/router";
-import {MatDialog} from "@angular/material/dialog";
-import {UserSettingsDialogComponent} from "../user-settings-dialog/user-settings-dialog.component";
 import {UserService} from "../../services/user.service";
-import {Subject, takeUntil} from "rxjs";
 import {FormControl} from "@angular/forms";
 
 @Component({
@@ -16,76 +12,34 @@ import {FormControl} from "@angular/forms";
   styleUrls: ['./drawer.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DrawerComponent implements OnInit, OnDestroy {
+export class DrawerComponent implements OnInit {
   @Input() tabs: TabInterface[] = [];
-  public avatar: string = '';
-  private notifier = new Subject<void>();
-  public loader: boolean = false;
   public searchInput = new FormControl('');
 
   constructor(
     public drawerService: DrawerService,
     public tokenService: TokenStorageService,
-    private alertService: AlertService,
     public router: Router,
-    private dialog: MatDialog,
     public userService: UserService,
-    private ref: ChangeDetectorRef
   ) {
   }
 
   ngOnInit(): void {
-    this.userService.getAvatar()
-      .pipe(takeUntil(this.notifier))
-      .subscribe(data => {
-        this.avatar = data;
-        this.ref.markForCheck();
-      });
-    this.userService.setAvatar(this.tokenService.getUser().img);
+
   }
 
   public close(): void {
     this.drawerService.setDrawer(false);
   }
 
-  public changeAvatar(event: Event): void {
-    this.loader = true
-    const file = (event.target as HTMLInputElement).files[0];
-    this.userService.uploadAvatar(file)
-      .pipe(takeUntil(this.notifier))
-      .subscribe((data) => {
-        this.tokenService.saveUser({...this.tokenService.getUser(), img: data.img})
-        this.loader = false
-        this.userService.setAvatar(data.img)
-        this.alertService.openSnackBar(data.message)
-      })
-  }
-
-  public signOut(): void {
-    this.tokenService.signOut();
-    this.drawerService.setDrawer(false);
-    this.alertService.openSnackBar('Successfully signed out!');
-    this.router.navigate(['/']);
-  }
-
-  public openSettings(): void {
-    this.dialog.open(UserSettingsDialogComponent, {
-      disableClose: true
-    })
-  }
-
   public search(): void {
+    this.searchInput.value !== '' &&
     this.router.navigate(['/all'], {
       queryParams: {
         title: this.searchInput.value
       }
     });
     this.drawerService.setDrawer(false);
-  }
-
-  ngOnDestroy() {
-    this.notifier.next()
-    this.notifier.complete()
   }
 
 }
--- front/src/app/shared/components/header/header.component.ts
+++ front/src/app/shared/components/header/header.component.ts
@@ -33,6 +33,7 @@ export class HeaderComponent implements OnInit {
   }
 
   public search(): void {
+    this.searchInput.value !== '' &&
     this.router.navigate(['/all'], {
       queryParams: {
         title: this.searchInput.value
